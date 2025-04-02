f2603c31bb2d970deca8b18144fdb95d0ea07b6b
Joseph Olawale Bamidele <47855279+jolawale28@users.noreply.github.com>
2025-04-02 14:49:19 +0100

    fix: login process

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Sidebar from './components/layout/SideBar'
 import { usePathname } from "next/navigation";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function AdminRootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 
@@ -18,43 +18,39 @@ export default function AdminRootLayout({ children }: Readonly<{ children: React
   };
 
   return (
-    <html lang='en'>
-      <body className={`antialiased`} >
-        <section className="bg-[url('/images/hero_wallpaper.png')] bg-cover bg-center flex gap-5 p-5 h-[calc(100vh)]">
-          <Sidebar onEdit={handleChanges} />
-          <main className="grow shrink h-full flex flex-col gap-y-10 w-full overflow-y-scroll scrollbar-hidden">
-            <div className="flex flex-row gap-5 justify-between md:items-center basis-auto">
+    <section className="bg-[url('/images/hero_wallpaper.png')] bg-cover bg-center flex gap-5 p-5 h-[calc(100vh)]">
+      <Sidebar onEdit={handleChanges} />
+      <main className="grow shrink h-full flex flex-col gap-y-10 w-full overflow-y-scroll scrollbar-hidden">
+        <div className="flex flex-row gap-5 justify-between md:items-center basis-auto">
 
-              <div className="flex md:flex-row flex-col justify-between gap-y-4 md:items-center w-full">
-                <div className="flex gap-x-3 items-center">
-                  <Link href="/admin">
-                    <div className="size-8 relative overflow-hidden">
-                      <Image src="/images/logo.png" alt="app_logo" fill objectFit="cover" />
-                    </div>
-                  </Link>
-
-                  {
-                    pathname === '/admin' ? (
-                      <div className="text-white">Welcome, Duv Mac</div>
-                    ) : (
-                      <ul className="text-white flex gap-x-1 items-center text-xs">
-                        <li><ChevronRight size={20} color="white" /></li>
-                        <li>Collections</li>
-                      </ul>
-                    )
-                  }
-
+          <div className="flex md:flex-row flex-col justify-between gap-y-4 md:items-center w-full">
+            <div className="flex gap-x-3 items-center">
+              <Link href="/admin">
+                <div className="size-8 relative overflow-hidden">
+                  <Image src="/images/logo.png" alt="app_logo" fill objectFit="cover" />
                 </div>
+              </Link>
 
-              </div>
+              {
+                pathname === '/admin' ? (
+                  <div className="text-white">Welcome, Duv Mac</div>
+                ) : (
+                  <ul className="text-white flex gap-x-1 items-center text-xs">
+                    <li><ChevronRight size={20} color="white" /></li>
+                    <li>Collections</li>
+                  </ul>
+                )
+              }
 
-              <MobileSideBar />
             </div>
 
-            {children}
-          </main>
-        </section>
-      </body>
-    </html>
+          </div>
+
+          <MobileSideBar />
+        </div>
+
+        {children}
+      </main>
+    </section>
   )
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,9 +3,24 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
 import signInUser from "@/app/firebase/signin_func";
-import {CardContent} from "@/app/components/ui/card content";
-import {Card} from "@/app/components/ui/card";
+import { CardContent } from "@/app/components/ui/card content";
+import { Card } from "@/app/components/ui/card";
 import { Button } from '../components/ui/button';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const SignInScreen = () => {
     const [email, setEmail] = useState('');
@@ -16,14 +31,20 @@ const SignInScreen = () => {
 
     const handleSignIn = async () => {
         setLoading(true);
-        try {
-            await signInUser(email, password);
-            router.push('/admin');
-        } catch {
-            setError('Invalid credentials or network error.');
-        } finally {
-            setLoading(false);
-        }
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                router.push('/admin')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(`${errorCode}: ${errorMessage}`);
+            }).finally(() => {
+                setLoading(false);
+            })
     };
 
     return (
